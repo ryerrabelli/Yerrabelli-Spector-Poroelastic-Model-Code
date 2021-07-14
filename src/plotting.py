d1125cb0ebfe470707472f9bceec261cce4767ce
Rahul Yerrabelli <ryerrabelli@gmail.com>
2021-07-14 00:00:21 -0500

    Redrew plots and sent to Dr. Spector

--- src/plotting.py
+++ src/plotting.py
@@ -36,6 +36,7 @@ def plot_laplace_analysis(func,
                           input_s,  # input_s should include all values of plot_s
                           input_times,  # input_times should include all values of plot_times
                           plot_s=None,
+                          plot_s_s=None,
                           plot_times=None,
                           time_const=1,  # default is times area already nondimensional, otherwise, divide by this to become nondimensional
                           input_times_anal=None,
@@ -46,6 +47,8 @@ def plot_laplace_analysis(func,
                           model_name=None,
                           func_labels=None,
                           legends_to_show_ct=1,
+                          legend_fontsize="small",  # string values for fontsize are relative, while int are absolute in pts. String values: {'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'}
+                          do_plot_laplace_times_s=True,
                           ):
 
     if plot_times is None:
@@ -66,8 +69,14 @@ def plot_laplace_analysis(func,
     funcs_ct = len(funcs)
     legends_shown_ct = 0
 
+    # Makes assumptions of the order of input_t and input_s having the plotting values at certain points
     plot_times_indices_in_input = np.arange(len(plot_times))
     plot_s_indices_in_input = np.arange(len(plot_s))
+    if plot_s_s is None:
+        plot_s_s = plot_s
+        plot_s_s_indices_in_input = plot_s_indices_in_input
+    else:
+        plot_s_s_indices_in_input = len(plot_s) + np.arange(len(plot_s_s))
 
     # Non-positive s values give an error "invalid value encountered in sqrt"
     laplace_vals_all = [func(input_s) for func in funcs]
@@ -98,7 +107,7 @@ def plot_laplace_analysis(func,
                                    / inverted_vals_analytical[is_in_num_times_too] for inverted_vals_numerical,inverted_vals_analytical in zip(inverted_vals_numerical_all,inverted_vals_analytical_all)  ]
 
     # Plotting
-    subplot_row_ct = 1 if all(inv_func_anal is None for inv_func_anal in inv_funcs_anal) else 2
+    subplot_row_ct = 1 if all(inv_func_anal is None for inv_func_anal in inv_funcs_anal) and not do_plot_laplace_times_s else 2
     #subplot_row_ct = 1
     subplot_col_ct = 2
     fig, axs = plt.subplots(subplot_row_ct, subplot_col_ct)
@@ -112,11 +121,12 @@ def plot_laplace_analysis(func,
         fig.tight_layout()  # defaults to using the entire figure
         fig.set_figheight(  4*subplot_row_ct-1 )
     fig.set_dpi(150)
-    fig.set_figwidth(9)
+    fig.set_figwidth(4.5 * subplot_col_ct)
 
     if subplot_row_ct == 1:
-        ax00 = axs[0]
-        ax01 = axs[1]
+        ax00 = axs[0] if subplot_col_ct >= 1 else None
+        ax01 = axs[1] if subplot_col_ct >= 2 else None
+        axO2 = axs[2] if subplot_col_ct >= 3 else None
     else:
         ax00 = axs[0, 0]
         ax01 = axs[0, 1]
@@ -127,7 +137,7 @@ def plot_laplace_analysis(func,
         ax00.plot(plot_s, laplace_vals[plot_s_indices_in_input], ".-b" if funcs_ct == 1 else ".-", label=func_label)
         #ax00.plot(input_s, laplace_vals*input_s, ".-b")
     if legends_shown_ct < legends_to_show_ct and any(func_label is not None for func_label in func_labels):
-        ax00.legend()
+        ax00.legend(fontsize=legend_fontsize)   # fontsize argument only used if prop argument is not specified.
         legends_shown_ct += 1
     ax00.set_xlabel(x_names["s"])
     # theoretically, there should be no lower limit on s, but non-positive values throw an error in the function
@@ -144,7 +154,7 @@ def plot_laplace_analysis(func,
     for inverted_vals_numerical, func_label in zip(inverted_vals_numerical_all, func_labels):
         ax01.plot(plot_times/time_const, inverted_vals_numerical[plot_times_indices_in_input], ".-r" if funcs_ct == 1 else ".-", label=func_label)
     if legends_shown_ct < legends_to_show_ct and any(func_label is not None for func_label in func_labels):
-        ax01.legend()
+        ax01.legend(fontsize="small")  # string values for fontsize are relative, while int are absolute
         legends_shown_ct += 1
     ax01.set_xlabel(x_names["t"])
     ax01.set_xlim([0, max(plot_times / time_const)])
@@ -154,6 +164,23 @@ def plot_laplace_analysis(func,
     ax01.grid(which="minor", alpha=0.75, linestyle=":")
     ax01.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
     ax01.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+
+    if do_plot_laplace_times_s:
+        for laplace_vals, func_label in zip(laplace_vals_all, func_labels):
+            ax10.plot(plot_s_s, plot_s_s*laplace_vals[plot_s_s_indices_in_input], ".-b" if funcs_ct == 1 else ".-", label=func_label)
+        if legends_shown_ct < legends_to_show_ct and any(func_label is not None for func_label in func_labels):
+            ax10.legend(fontsize=legend_fontsize)   # fontsize argument only used if prop argument is not specified.
+            legends_shown_ct += 1
+        ax10.set_xlabel(x_names["s"])
+        # theoretically, there should be no lower limit on s, but non-positive values throw an error in the function
+        # ax00.set_xlim([0, None])
+        ax10.set_xlim([0, max(plot_s_s)])
+        ax10.set_ylabel(r"$s\cdot$"+func_name["s"])
+        ax10.title.set_text("Laplace x s")
+        ax10.grid(which="major")
+        ax10.grid(which="minor", alpha=0.75, linestyle=":")
+        ax10.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+        ax10.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
 
     if any(inverted_vals_analytical is not None for inverted_vals_analytical in inverted_vals_analytical_all):
         for inverted_vals_analytical in inverted_vals_analytical_all:
@@ -167,39 +194,44 @@ def plot_laplace_analysis(func,
         ax11.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
         ax11.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
 
-        if is_in_anal_times_too.any():
-            for inversion_error in inversion_error_all:
-                #ax10.plot(plot_times_anal[is_in_num_times_too], inversion_error * 100.0, ".-g")
-                ax10.plot(plot_times_anal[is_in_num_times_too], inversion_error, ".-g" if funcs_ct == 1 else ".-")
-            if all(min(abs(inversion_error * 100.0)) < 0.1 for inversion_error in inversion_error_all):
-                ax10.set_ylim([-100, 100])
-            else:
-                # Center y axis around 0
-                yabs_max = abs(max(ax10.get_ylim(), key=abs))*1.1
-                ax10.set_ylim(bottom=-yabs_max, top=yabs_max)
-                """ 
-                # Force 0 to be in the y axis range
-                if max(ax10.get_ylim()) < 0:
-                    ax10.set_ylim(top=0)
-                elif min(ax10.get_ylim()) > 0:
-                    ax10.set_ylim(bottom=0)
-                """
-        else:
-            ax10.set_ylim([-100, 100])
-        ax10.set_xlabel(x_names["t"])
-        ax10.set_xlim([0, None])
-        ax10.set_ylabel(func_name["t"])
-        #ax10.set_ylabel("% error: (Numer-Anal)/Anal")
-        ax10.set_ylabel(r"% $error=\frac{f_{Numer}(t)-f_{Anal}(t)}{f_{Anal}(t)}$")
-        # Note- if a y value is 0.5, then percentformatter makes it 0.5% not 50%.
-        # Thus have to multiply by 100 before getting the y values.
-        ax10.yaxis.set_major_formatter(matplotlib.ticker.PercentFormatter())
 
-        ax10.title.set_text("Percent Error")
-        ax10.grid(which="major")
-        ax10.grid(which="minor", alpha=0.75, linestyle=":")
-        ax10.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
-        ax10.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+        if not do_plot_laplace_times_s:
+            if is_in_anal_times_too.any():
+                for inversion_error in inversion_error_all:
+                    #ax10.plot(plot_times_anal[is_in_num_times_too], inversion_error * 100.0, ".-g")
+                    ax10.plot(plot_times_anal[is_in_num_times_too], inversion_error, ".-g" if funcs_ct == 1 else ".-")
+                if all(min(abs(inversion_error * 100.0)) < 0.1 for inversion_error in inversion_error_all):
+                    ax10.set_ylim([-100, 100])
+                else:
+                    # Center y axis around 0
+                    yabs_max = abs(max(ax10.get_ylim(), key=abs))*1.1
+                    ax10.set_ylim(bottom=-yabs_max, top=yabs_max)
+                    """ 
+                    # Force 0 to be in the y axis range
+                    if max(ax10.get_ylim()) < 0:
+                        ax10.set_ylim(top=0)
+                    elif min(ax10.get_ylim()) > 0:
+                        ax10.set_ylim(bottom=0)
+                    """
+            else:
+                ax10.set_ylim([-100, 100])
+            ax10.set_xlabel(x_names["t"])
+            ax10.set_xlim([0, None])
+            ax10.set_ylabel(func_name["t"])
+            #ax10.set_ylabel("% error: (Numer-Anal)/Anal")
+            ax10.set_ylabel(r"% $error=\frac{f_{Numer}(t)-f_{Anal}(t)}{f_{Anal}(t)}$")
+            # Note- if a y value is 0.5, then percentformatter makes it 0.5% not 50%.
+            # Thus have to multiply by 100 before getting the y values.
+            ax10.yaxis.set_major_formatter(matplotlib.ticker.PercentFormatter())
+
+            ax10.title.set_text("Percent Error")
+            ax10.grid(which="major")
+            ax10.grid(which="minor", alpha=0.75, linestyle=":")
+            ax10.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+            ax10.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+
+    elif subplot_row_ct >= 2:
+        ax11.axis("off")
 
     assert all(len(data) == funcs_ct for data in [laplace_vals_all,inverted_vals_numerical_all,inverted_vals_analytical_all])
     if return_singles:
@@ -209,5 +241,5 @@ def plot_laplace_analysis(func,
         laplace_vals_all = laplace_vals_all.pop()
         inverted_vals_numerical_all = inverted_vals_numerical_all.pop()
         inverted_vals_analytical_all = inverted_vals_analytical_all.pop()
-    return fig, laplace_vals_all, inverted_vals_numerical_all, inverted_vals_analytical_all
+    return fig, axs, laplace_vals_all, inverted_vals_numerical_all, inverted_vals_analytical_all
 
